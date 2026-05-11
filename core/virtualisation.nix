6adf5d21f73085176c1a587a76ff6a96b34ed839
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-05-11 15:29:53 +1000

    extra settings for podman

--- core/virtualisation.nix
+++ core/virtualisation.nix
@@ -1,24 +1,24 @@
 {...}: {
   programs.virt-manager.enable = true;
-
   virtualisation = {
     podman = {
       enable = true;
       dockerCompat = true;
-
       defaultNetwork.settings = {
         dns_enabled = true;
       };
     };
-
     libvirtd = {
       enable = true;
-
       qemu = {
         swtpm.enable = true;
       };
     };
-
     spiceUSBRedirection.enable = true;
   };
+
+  environment.etc."containers/registries.conf".text = ''
+    [registries.search]
+    registries = ["docker.io"]
+  '';
 }
