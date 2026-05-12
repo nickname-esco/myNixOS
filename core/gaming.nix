4585dcf3615b25800b470cbb4c5379c442c25eab
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-05-12 23:44:35 +1000

    fixing for the ninth zilliionnnnnnnnnnnnnnnnnnnnnnnnnnth time lol

--- core/gaming.nix
+++ core/gaming.nix
@@ -1,16 +1,18 @@
-{
-  pkgs,
-  ...
-}:
-{
+{pkgs, ...}: {
   programs = {
     steam = {
       enable = true;
+
+      package = pkgs.steam.override {
+        extraArgs = "-cef-disable-gpu-compositing";
+      };
+
       gamescopeSession.enable = true;
-      extraCompatPackages = [ pkgs.proton-ge-bin ];
+      extraCompatPackages = [pkgs.proton-ge-bin];
     };
 
     gamemode.enable = true;
+
     gamescope = {
       enable = true;
       capSysNice = true;
