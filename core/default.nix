84e6a415deebbfeeb635c7db4c458513d84c5aac
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-05-12 20:26:29 +1000

    adding niri + noctalia x3

--- core/default.nix
+++ core/default.nix
@@ -1,6 +1,6 @@
-{ ... }:
-{
+{...}: {
   imports = [
+    ./niri
     ./user.nix
     ./boot.nix
     ./unfree.nix
--- core/desktop.nix
+++ core/desktop.nix
@@ -24,36 +24,7 @@
         enable = true;
       };
     };
-
-    # Needed by desktop shells, portals, auth prompts, etc.
-    gnome.gnome-keyring.enable = true;
   };
-
-  # Niri Wayland compositor.
-  programs.niri = {
-    enable = true;
-  };
-
-  # Needed for privilege/auth prompts in graphical sessions.
-  security.polkit.enable = true;
-
-  # Desktop portals for file pickers, screen sharing, app integration, etc.
-  xdg.portal = {
-    enable = true;
-
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome
-    ];
-  };
-
-  # Useful system-level Wayland/niri support packages.
-  environment.systemPackages = with pkgs; [
-    xwayland-satellite
-    xdg-desktop-portal
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-gnome
-  ];
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     kate
