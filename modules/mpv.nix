bbb68d49e46784a6cbb6bd11511e7db89628b905
nickname-esco <***REDACTED***>
2026-05-13 22:27:49 +1000

    improve mpv

--- modules/mpv.nix
+++ modules/mpv.nix
@@ -1,8 +1,26 @@
 {pkgs, ...}: {
-  programs = {
-    mpv = {
-      enable = true;
-      scripts = [pkgs.mpvScripts.uosc];
+  programs.mpv = {
+    enable = true;
+
+    scripts = with pkgs.mpvScripts; [
+      uosc
+      thumbfast
+      mpris
+      sponsorblock-minimal
+    ];
+
+    config = {
+      # Needed because uosc replaces mpv's default on-screen controls
+      osc = false;
+
+      # Better video output
+      vo = "gpu-next";
+      gpu-api = "auto";
+
+      # Good defaults
+      save-position-on-quit = true;
+      keep-open = true;
+      autofit-larger = "90%x90%";
     };
   };
 }
