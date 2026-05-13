0bfcd6425a6df0687e65a55864a1518f37140145
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-05-13 22:44:49 +1000

    improve mpv x2

--- modules/mpv.nix
+++ modules/mpv.nix
@@ -10,17 +10,27 @@
     ];
 
     config = {
-      # Needed because uosc replaces mpv's default on-screen controls
       osc = false;
 
-      # Better video output
       vo = "gpu-next";
       gpu-api = "auto";
 
-      # Good defaults
       save-position-on-quit = true;
       keep-open = true;
       autofit-larger = "90%x90%";
+
+      # Better YouTube defaults
+      ytdl-format = "bestvideo[height<=1080]+bestaudio/best[height<=1080]/best";
+    };
+
+    scriptOpts = {
+      ytdl_hook = {
+        ytdl_path = "${pkgs.yt-dlp}/bin/yt-dlp";
+      };
     };
   };
+
+  home.packages = with pkgs; [
+    yt-dlp
+  ];
 }
