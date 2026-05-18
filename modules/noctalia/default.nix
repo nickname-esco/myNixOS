e43c9d7d8517625924c0e2b20c4961fe49038e12
nickname-esco <***REDACTED***>
2026-05-18 19:19:02 +1000

    rm duplicates--mainly location + weather

--- modules/noctalia/default.nix
+++ modules/noctalia/default.nix
@@ -5,15 +5,6 @@
 }: let
   noctaliaSettings =
     builtins.fromJSON (builtins.readFile ./settings.json);
-
-  weatherWidget = {
-    id = "Weather";
-  };
-
-  rightWidgets =
-    if builtins.any (widget: (widget.id or "") == "Weather") noctaliaSettings.bar.widgets.right
-    then noctaliaSettings.bar.widgets.right
-    else [weatherWidget] ++ noctaliaSettings.bar.widgets.right;
 in {
   imports = [
     inputs.noctalia.homeModules.default
@@ -25,23 +16,10 @@ in {
     settings = lib.recursiveUpdate noctaliaSettings {
       bar = {
         backgroundOpacity = 0.75;
-
-        widgets = {
-          right = rightWidgets;
-        };
       };
 
       ui = {
         panelBackgroundOpacity = 0.82;
-      };
-
-      location = {
-        name = "Melbourne";
-        weatherEnabled = true;
-        weatherShowEffects = true;
-        useFahrenheit = false;
-        use12hourFormat = true;
-        autoLocate = true;
       };
 
       colorSchemes = {
