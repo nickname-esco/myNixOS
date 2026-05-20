060bad5b234f5ad7b7cc39bf5aa2dc3fcbdabd87
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-05-20 23:42:55 +1000

    adding secrets x4

---
+++ secrets/duckdns.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  systemd = {
+    services.duckdns = {
+      description = "DuckDNS Dynamic DNS Updater";
+      after = ["network-online.target"];
+      wants = ["network-online.target"];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = pkgs.writeShellScript "duckdns-update" ''
+          TOKEN=$(cat /etc/duckdns/token)
+          ${pkgs.curl}/bin/curl -s \
+            "https://www.duckdns.org/update?domains=mynixos&token=$TOKEN&ip="
+        '';
+      };
+    };
+
+    timers.duckdns = {
+      description = "DuckDNS Update Timer";
+      wantedBy = ["timers.target"];
+      timerConfig = {
+        OnBootSec = "1min";
+        OnUnitActiveSec = "5min";
+        Unit = "duckdns.service";
+      };
+    };
+  };
+}
