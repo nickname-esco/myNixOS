9c76ed92df1cc344411779b7f13ab76729ba7f10
nickname-esco <***REDACTED***>
2026-05-22 17:22:28 +1000

    moving wireguard to default.nix

--- wireguard/default.nix
+++ wireguard/default.nix
@@ -1,6 +1,35 @@
-{
+{pkgs, ...}: {
   imports = [
     ./duckdns.nix
-    ./wireguard.nix
   ];
+
+  environment.systemPackages = [pkgs.wireguard-tools];
+
+  networking = {
+    nat = {
+      enable = true;
+      externalInterface = "wlo1";
+      internalInterfaces = ["wg0"];
+    };
+
+    wireguard.interfaces = {
+      wg0 = {
+        ips = ["10.0.0.1/24"];
+        listenPort = 51820;
+        privateKeyFile = "/etc/wireguard/server.key";
+
+        peers = [
+          {
+            # Phone
+            publicKey = "SWu37BJZWoymIkjyWVPQqhjquMdrOqJp3n+5zMdlsyE=";
+            allowedIPs = ["10.0.0.2/32"];
+          }
+        ];
+      };
+    };
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+  };
 }
