64b9605790b532ce799b8ba438c9a7ba4910aefc
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-06-25 01:06:54 +1000

    import home features directly

--- home.nix
+++ home.nix
@@ -1,6 +1,30 @@
 {...}: {
   imports = [
-    ./home
+    ./features/base/home-unfree.nix
+
+    ./features/desktop/gtk.nix
+    ./features/noctalia
+
+    ./features/terminal/omp
+    ./features/terminal/kitty.nix
+    ./features/terminal/fish.nix
+    ./features/terminal/tmux.nix
+    ./features/terminal/bat.nix
+    ./features/terminal/eza.nix
+    ./features/terminal/direnv.nix
+    ./features/terminal/nh.nix
+
+    ./features/editor/nixvim.nix
+
+    ./features/dev/git.nix
+    ./features/dev/lazygit.nix
+    ./features/dev/devkit.nix
+
+    ./features/shell/fastfetch
+    ./features/shell/htop.nix
+    ./features/shell/utils.nix
+
+    ./features/media/mpv.nix
   ];
 
   home = {
