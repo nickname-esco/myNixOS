075bb67d3ec2ff0da3ce1f8e472dcd3c13306322
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-06-25 14:22:25 +1000

    fix git contribution email

--- features/dev/git.nix
+++ features/dev/git.nix
@@ -2,6 +2,9 @@
   programs.git = {
     enable = true;
 
+    userName = "nickname-esco";
+    userEmail = "133730657+nickname-esco@users.noreply.github.com";
+
     settings = {
       push.default = "simple";
       credential.helper = "cache --timeout=7200";
@@ -9,11 +12,6 @@
       log.decorate = "full";
       log.date = "iso";
       merge.conflictstyle = "diff3";
-
-      user = {
-        name = "nickname-esco";
-        email = "***REDACTED***";
-      };
     };
   };
 }
