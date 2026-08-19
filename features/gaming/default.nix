1b93990e24c2628f54fc6397050a1477295fa88f
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-08-19 23:41:08 +1000

    update, and rm steam gamescope session

--- features/gaming/default.nix
+++ features/gaming/default.nix
@@ -10,7 +10,7 @@
   programs = {
     steam = {
       enable = true;
-      gamescopeSession.enable = true;
+      gamescopeSession.enable = false;
       extraCompatPackages = [pkgs.proton-ge-bin];
     };
 
