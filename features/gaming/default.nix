5e8b811dc496589a1d09e1740602d6d49680f4af
nickname-esco <133730657+nickname-esco@users.noreply.github.com>
2026-08-19 23:57:03 +1000

    update, and rm override steam tag (-cef-disable-gpu-compositing)

--- features/gaming/default.nix
+++ features/gaming/default.nix
@@ -1,12 +1,4 @@
 {pkgs, ...}: {
-  nixpkgs.overlays = [
-    (final: prev: {
-      steam = prev.steam.override {
-        extraArgs = "-cef-disable-gpu-compositing";
-      };
-    })
-  ];
-
   programs = {
     steam = {
       enable = true;
